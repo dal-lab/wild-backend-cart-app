f5cc5514d94cfcd8f3b7ae5f0c74366ba045be9f
songtov <songchiho777@gmail.com>
2024-12-27 22:53:48 +0900

    Add DAO and tests

--- src/main/java/com/example/demo/controllers/CartController.java
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -1,64 +1,42 @@
 package com.example.demo.controllers;
 
+import com.example.demo.application.CartService;
 import com.example.demo.controllers.dtos.CartDto;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.model.Filters;
-import org.bson.Document;
-import org.bson.types.ObjectId;
+import com.example.demo.models.Cart;
+import com.example.demo.models.LineItem;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/cart")
 public class CartController {
+    private final CartService cartService;
 
-    private final MongoDatabase mongoDatabase;
-
-    public CartController(MongoDatabase mongoDatabase) {
-        this.mongoDatabase = mongoDatabase;
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
     }
+
 
     @GetMapping
     CartDto detail() {
-        MongoCollection<Document> collection =
-                mongoDatabase.getCollection("lineItems");
-
-
-        List<Document> documents = new ArrayList<>();
-        collection.find().into(documents);
+        Cart cart = cartService.getCart();
 
         return new CartDto(
-                documents.stream().map(
-                        document -> {
-                            MongoCollection<Document> productCollection =
-                                    mongoDatabase.getCollection("products");
-                            String productId = document.getString("product_Id");
-
-                            Document productDocument = productCollection.find(
-                                    Filters.eq("_id", new ObjectId(productId))
-                            ).first();
-
-                            //TODO: need to handle execption if productDocument is null
-
-
-                            int unitPrice = productDocument.getInteger("price");
-                            int quantity = document.getInteger("quantity");
-                            return new CartDto.LineItemDto(
-                                    document.getObjectId("_id").toString(),
-                                    document.getString("product_Id"),
-                                    productDocument.getString("name"),
-                                    unitPrice,
-                                    quantity,
-                                    unitPrice * quantity
-                            );
-
-                        }).toList(),
-                0
+                cart.getLineItems().stream().map(this::mapToDto).toList(),
+                cart.getTotalPrice()
         );
     }
+
+    private CartDto.LineItemDto mapToDto(LineItem lineItem) {
+        return new CartDto.LineItemDto(
+                lineItem.getId(),
+                lineItem.getProductId(),
+                lineItem.getProductName(),
+                lineItem.getUnitPrice(),
+                lineItem.getQuantity(),
+                lineItem.getTotalPrice()
+        );
+    }
+
 }
---
+++ src/main/java/com/example/demo/infrastructure/ProductDAO.java
@@ -0,0 +1,35 @@
+package com.example.demo.infrastructure;
+
+import com.example.demo.models.Product;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDAO {
+    private final MongoCollection<Document> collection;
+
+    public ProductDAO(MongoDatabase mongoDatabase) {
+        this.collection = mongoDatabase.getCollection("line_items");
+    }
+
+
+    public Product find(String productId) {
+        Document document = collection.find(
+                Filters.eq("_id", new ObjectId(productId))
+        ).first();
+
+        if (document == null) {
+            return null;
+        }
+
+        return new Product(
+                document.getObjectId("_id").toString(),
+                document.getString("name"),
+                document.getInteger("price")
+        );
+    }
+}
